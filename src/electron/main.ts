a4aca697f1a1f33fd1675c1f457f81285adba3fd
yash.agrawal <yash.agrawal@bookmyshow.com>
2025-07-27 03:53:55 +0530

    told tsconfig the proper files to include/exclude to prevent lack of memory, also created build config for electron-builder for appId and to tell which files to include for build and in what format should it be available in different OS to install

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,9 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
+
 type some = String;
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({
-    
   });
 
   mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html")); 
